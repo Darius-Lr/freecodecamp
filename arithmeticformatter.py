e9175f5f7576ff7bca0630d5e308f83bf0437961
Lare Claudiu-Darius <foamesiambitie@gmail.com>
2025-08-23 14:01:18 +0300

    finished

--- arithmeticformatter.py
+++ arithmeticformatter.py
@@ -5,7 +5,7 @@ def arithmetic_arranger(problems,show=False):
     firstline=""
     secondline=""
     thirdline=""
-
+    fourline=""
     for x in problems:
         string=string+x+" "
     parts=string.split()
@@ -19,13 +19,27 @@ def arithmetic_arranger(problems,show=False):
             return 'Error: Numbers cannot be more than four digits.'
         if not parts[i].isdigit() or not parts[i+2].isdigit():
             return 'Error: Numbers must only contain digits.'
+
         maxn=max(len(parts[i]),len(parts[i+2]))
         help=str(parts[i]).rjust(maxn+2)
+        if parts[i+1]=="+":
+            score=int(parts[i])+int(parts[i+2])
+            fourline=fourline+str(score).rjust(maxn+2)+"    "
+        else:
+            score = int(parts[i]) - int(parts[i + 2])
+            fourline = fourline + str(score).rjust(maxn + 2) + "    "
         firstline=firstline+help+"    "
         help=str(parts[i+1])+str(parts[i+2]).rjust(maxn+1)
         secondline=secondline+help+"    "
         thirdline=thirdline+'-'*(maxn+2)+"    "
+    firstline=firstline[:-4]
+    secondline = secondline[:-4]
+    thirdline = thirdline[:-4]
+    fourline=fourline[:-4]
+
     result=firstline+'\n'+secondline+'\n'+thirdline
+    if show==True:
+        result=result+'\n'+fourline
     return result
 
 
@@ -33,4 +47,3 @@ def arithmetic_arranger(problems,show=False):
 if __name__ == '__main__':
     result=arithmetic_arranger(["1 + 2", "1 - 9380"])
     print(result)
-    print("  1         1\n+ 2    - 9380\n---    ------")
