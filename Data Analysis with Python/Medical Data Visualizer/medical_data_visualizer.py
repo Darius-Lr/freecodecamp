4732846347467937fc29c1410266075aae5d909d
Lare Claudiu-Darius <foamesiambitie@gmail.com>
2025-09-29 22:03:20 +0300

    fixed the issues

--- Data Analysis with Python/Medical Data Visualizer/medical_data_visualizer.py
+++ Data Analysis with Python/Medical Data Visualizer/medical_data_visualizer.py
@@ -1,7 +1,10 @@
-import pandas as pd
-import seaborn as sns
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
 import numpy as np
+
 
 # 1
 df = pd.read_csv("medical_examination.csv")
@@ -9,29 +12,34 @@ df = pd.read_csv("medical_examination.csv")
 # 2
 df["BMI"] = df["weight"] / ((df["height"] / 100) ** 2)
 df["overweight"] = df["BMI"].apply(lambda x: 1 if x > 25 else 0)
-
+df.drop('BMI', axis=1, inplace=True)
 
 # 3
-df['cholesterol']=df['cholesterol'].apply(lambda x:1 if x>1 else 0)
-df['gluc']=df['gluc'].apply(lambda x:1 if x>1 else 0)
+df['cholesterol'] = df['cholesterol'].apply(lambda x: 1 if x > 1 else 0)
+df['gluc'] = df['gluc'].apply(lambda x: 1 if x > 1 else 0)
+
 
 # 4
 def draw_cat_plot():
     # 5
-    df_cat = None
-
+    df_cat = pd.melt(df, id_vars=['cardio'],
+                     value_vars=['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight'])
 
     # 6
-    df_cat = None
-    
+
+    df_cat = (
+        df_cat
+        .groupby(['cardio', 'variable', 'value'])
+        .size()
+        .reset_index(name='total')
+    )
 
     # 7
 
-
+    g = sns.catplot(data=df_cat, x='variable', y='total', hue='value', col='cardio', kind='bar')
 
     # 8
-    fig = None
-
+    fig = g.fig
 
     # 9
     fig.savefig('catplot.png')
@@ -41,22 +49,27 @@ def draw_cat_plot():
 # 10
 def draw_heat_map():
     # 11
-    df_heat = None
-
+    height_low = df['height'].quantile(0.025)
+    height_high = df['height'].quantile(0.975)
+    weight_low = df['weight'].quantile(0.025)
+    weight_high = df['weight'].quantile(0.975)
+    df_heat = df[
+        (df['ap_lo'] < df['ap_hi']) &
+        (df['height'].between(height_low, height_high)) &
+        (df['weight'].between(weight_low, weight_high))
+        ]
     # 12
-    corr = None
+    corr = df_heat.corr()
 
     # 13
-    mask = None
-
-
+    mask = np.triu(np.ones_like(corr, dtype=bool))
 
     # 14
-    fig, ax = None
+    fig, ax = plt.subplots(figsize=(12, 12))
 
     # 15
 
-
+    sns.heatmap(corr, mask=mask, annot=True, fmt=".1f", cmap="crest")
 
     # 16
     fig.savefig('heatmap.png')
